619bdb0a59fa3b77e453c3e4073f9b496249afb3
Kingsley6624 <chineduezenwafor2@gmail.com>
2025-09-02 14:13:44 +0100

    con=mpleted testimonia section and beginning footer section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection'
 import Features from '../components/Features'
 import BlogPosts from '../components/BlogPosts'
 import HomeResources from '../components/homeComponents/HomeResources'
+import Review from '../components/homeComponents/Review'
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Features />
       <BlogPosts />
       <HomeResources />
+      <Review />
       </div>
   )
 }
